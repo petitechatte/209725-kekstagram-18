2a045f2daf31f5f16cbd6e1ec6048ed70c415763
petitechatte <natalyavaskot@gmail.com>
2019-10-14 22:01:57 +0300

    Добавлен выбор случайных фотографий

--- js/gallery.js
+++ js/gallery.js
@@ -54,7 +54,7 @@
   var getPhotos = function (response) {
     renderPhotos(response);
     // Показываем фильтры для сортировки
-    window.showFilters();
+    window.activateFilters();
     // Экспортируем данные для сортировки
     window.gallery = {
       // Сохраняем исходный массив данных после загрузки
--- js/sorting.js
+++ js/sorting.js
@@ -3,15 +3,56 @@
 'use strict';
 
 (function () {
+  // Количество случайных фотографий
+  var SELECTED_PHOTOS_NUMBER = 10;
+
+  // Элементы DOM
   var filtersBlock = document.querySelector('.img-filters');
   var filterPopular = document.querySelector('#filter-popular');
   var filterRandom = document.querySelector('#filter-random');
   var filterDiscussed = document.querySelector('#filter-discussed');
 
-  window.showFilters = function () {
+  var randomPhotos = [];
+
+  // Получаем случайное значение из массива
+
+  var getRandomValue = function (data) {
+    return data[Math.floor(Math.random() * (data.length))];
+  };
+
+  // Выбираем элементы для массива заданной длины из другого массива
+
+  var selectData = function (data, elementsNumber) {
+    var selectedElements = [];
+    var element;
+
+    while (selectedElements.length < elementsNumber) {
+      element = getRandomValue(data);
+
+      if (selectedElements.indexOf(element) === -1) {
+        selectedElements.push(element);
+      }
+    }
+
+    return selectedElements;
+  };
+
+  // Подбираем случайные фотографии
+
+  var getRandomPhotos = function () {
+    randomPhotos = selectData(window.gallery.initialData, SELECTED_PHOTOS_NUMBER);
+    window.gallery.updatePhotos(randomPhotos);
+  };
+
+  window.activateFilters = function () {
+    // Показываем кнопки-фильтры
     filtersBlock.classList.remove('img-filters--inactive');
+    // Добвляем кнопкам обработчики
     filterPopular.addEventListener('click', function () {
       window.gallery.updatePhotos(window.gallery.initialData);
+    });
+    filterRandom.addEventListener('click', function () {
+      getRandomPhotos();
     });
   };
 })();
