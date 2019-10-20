46b3401b5d99575a1bf7f95bf906e451088f7f89
petitechatte <natalyavaskot@gmail.com>
2019-10-20 19:44:21 +0300

    Добавлено описание и комментарии к полноразмерной фотографии

--- js/full-view.js
+++ js/full-view.js
@@ -6,16 +6,49 @@
   // Находим элементы в галерее
   var fullViewPopup = document.querySelector('.big-picture');
   var fullViewPhoto = fullViewPopup.querySelector('.big-picture__img img');
-  var fullViewPhotoLikes = fullViewPopup.querySelector('.likes-count');
-  var fullViewPhotoCommentsNumber = fullViewPopup.querySelector('.comments-count');
+  var fullViewHeading = fullViewPopup.querySelector('.social__caption');
+  var fullViewLikes = fullViewPopup.querySelector('.likes-count');
+  var fullViewCommentsNumber = fullViewPopup.querySelector('.comments-count');
+  var fullViewCommentsList = fullViewPopup.querySelector('.social__comments');
+  var fullViewComment = fullViewCommentsList.querySelector('.social__comment');
+
+  // Создаем комментарии к посту
+
+  var createComments = function (comments) {
+    var fragment = document.createDocumentFragment();
+    var commentBlock;
+    var commentatorAvatar;
+    var commentText;
+
+    for (var i = 0; i < comments.length; i++) {
+      // Копируем разметку комментария
+      commentBlock = fullViewComment.cloneNode(true);
+      fullViewCommentsList.appendChild(commentBlock);
+      commentatorAvatar = commentBlock.querySelector('.social__picture');
+      commentText = commentBlock.querySelector('.social__text');
+      // Заполняем шаблон комментария данными
+      commentatorAvatar.src = comments[i].avatar;
+      commentatorAvatar.alt = comments[i].name;
+      commentText.textContent = comments[i].message;
+      // Добавляем комментарий в список
+      fragment.appendChild(commentBlock);
+    }
+
+    // Показываем список комментариев
+    fullViewCommentsList.innerHTML = '';
+    fullViewCommentsList.appendChild(fragment);
+  };
 
   // Показываем пост с полноразмерной фотографией
 
   window.showFullViewPopup = function (currentData) {
+    var post = currentData[0];
     fullViewPopup.classList.remove('hidden');
-    fullViewPhoto.src = currentData[0].url;
-    fullViewPhoto.alt = currentData[0].description;
-    fullViewPhotoLikes.textContent = currentData[0].likes;
-    fullViewPhotoCommentsNumber.textContent = currentData[0].comments.length;
+    fullViewPhoto.src = post.url;
+    fullViewPhoto.alt = post.description;
+    fullViewHeading.textContent = post.description;
+    fullViewLikes.textContent = post.likes;
+    fullViewCommentsNumber.textContent = post.comments.length;
+    createComments(post.comments);
   };
 })();
