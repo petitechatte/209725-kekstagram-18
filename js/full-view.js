23c5ba14bd3814b8a2d8a4bf77960068dfbcbd0d
petitechatte <natalyavaskot@gmail.com>
2019-10-20 18:48:09 +0300

    Добавлено отображение количества лайков и комментариев при просмотре первой фотографии

--- js/full-view.js
+++ js/full-view.js
@@ -6,9 +6,16 @@
   // Находим элементы в галерее
   var fullViewPopup = document.querySelector('.big-picture');
   var fullViewPhoto = fullViewPopup.querySelector('.big-picture__img img');
+  var fullViewPhotoLikes = fullViewPopup.querySelector('.likes-count');
+  var fullViewPhotoCommentsNumber = fullViewPopup.querySelector('.comments-count');
+
+  // Показываем пост с полноразмерной фотографией
 
   window.showFullViewPopup = function (currentData) {
     fullViewPopup.classList.remove('hidden');
     fullViewPhoto.src = currentData[0].url;
+    fullViewPhoto.alt = currentData[0].description;
+    fullViewPhotoLikes.textContent = currentData[0].likes;
+    fullViewPhotoCommentsNumber.textContent = currentData[0].comments.length;
   };
 })();
